4c99c12a449d860da9beb25a927ceea72cf26c69
Johan Arteaga <jaac219@gmail.com>
2022-09-29 16:56:12 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+  require("dotenv").config();
+const express = require("express");
+
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { ApolloServer } = require('apollo-server-express')
+const app = express();
+
+const PORT = process.env.PORT;
+
+const typeDefs = [`
+  type Query {
+    hello: String
+  }
+`]
+const resolvers = {
+  Query: {
+    hello: () => 'Hello'
+  }
+}
+
+app.get('/', (req, res) =>{
+  res.redirect(`http://localhost:${PORT}/graphql`);
+})
+
+async function start() {
+  const schema = makeExecutableSchema({typeDefs, resolvers})
+  const apolloServer = new ApolloServer({schema});
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({app})
+  app.listen(PORT, (req, res)=>{
+    console.log(`Servidor iniciado en el puerto ${PORT} 🚀`);
+  })
+}
+
+start();
