d306d0343253e57e4802806e463717461bba3a0f
Johan Arteaga <jaac219@gmail.com>
2022-10-26 09:19:31 -0500

    Se aplica plugin para permitir acceso remoto a los servicios

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 require("./src/db.js");
 
+
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { graphqlUploadExpress } = require("graphql-upload");
 const jwt = require("jsonwebtoken");
 
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
+const { ApolloServerPluginLandingPageProductionDefault } = require('apollo-server-core');
 
 const PORT = process.env.PORT;
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -17,6 +19,12 @@ const resolvers = require("./src/merge/mergeResolvers.js");
 
 app.use(express.static('public'));
 app.use(graphqlUploadExpress());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+  next()
+})
 
 // app.use(async (req, res, next)=>{
 //   let token = req.headers.authorization?.split(' ')[1] || "";
@@ -33,8 +41,13 @@ app.get('/', (req, res) =>{
 async function start() {
   const schema = makeExecutableSchema({typeDefs, resolvers})
   const apolloServer = new ApolloServer({ 
+    schema,
+    plugins: [
+      ApolloServerPluginLandingPageProductionDefault({ 
+        embed: true 
+      }),
+    ],
     context: ctx=>ctx,
-    schema
   });
   await apolloServer.start();
 
